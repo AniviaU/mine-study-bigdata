bfcb5ecec2e1db2fff449b59e25ed7d49d15bf4e
fengpeng <fp15830263785@163.com>
2020-03-24 09:17:15 +0800

    创建 JavaSE module。

---
+++ bigdata-javase/src/main/java/pers/fp/study/bagdata/javase/Test.java
@@ -0,0 +1,12 @@
+package pers.fp.study.bagdata.javase;
+
+/**
+ * @author fengpeng
+ * @date 2020/3/23 20:28
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(" hello world ");
+    }
+}
